b1393948af5e9315f09376b4104bba78d8a7b860
Adrian Voicu <avenir.ro@gmail.com>
2015-11-10 12:28:22 +0200

    ...
    ...

--- application/views/templates/_parts/public_master_footer_view.php
+++ application/views/templates/_parts/public_master_footer_view.php
@@ -4,6 +4,8 @@
         <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
     </div>
 </footer>
+<script src="<?php echo site_url('assets/js/jquery-2.1.4.min.js');?>"></script>
+<script src="<?php echo site_url('assets/js/bootstrap.min.js');?>"></script>
 <?php echo $before_closing_body;?>
 </body>
 </html>
--- application/views/user/login_view.php
+++ application/views/user/login_view.php
@@ -1,19 +1,30 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <div class="container">
-    <?php
-    echo isset($_SESSION['auth_message']) ? $_SESSION['auth_message'] : FALSE;
-    ?>
-    <h1>Login in</h1>
-    <?php
-    echo form_open();
-    echo form_label('Username:','username').'<br />';
-    echo form_error('username');
-    echo form_input('username').'<br />';
-    echo form_label('Password:', 'password').'<br />';
-    echo form_error('password');
-    echo form_password('password').'<br />';
-    echo form_checkbox('remember','1',FALSE).' Remember me<br />';
-    echo form_submit('submit','Log In');
-    echo form_close();
-    ?>
+    <div class="row">
+        <?php
+        echo isset($_SESSION['auth_message']) ? $_SESSION['auth_message'] : FALSE;
+        ?>
+        <h1>Login</h1>
+        <div class="col-lg-4 col-lg-offset-4">
+            <?php echo $this->session->flashdata('message');?>
+            <?php echo form_open('',array('class'=>'form-horizontal'));?>
+            <div class="form-group">
+                <?php echo form_label('Username','username');?>
+                <?php echo form_error('username');?>
+                <?php echo form_input('username','','class="form-control"');?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Password','password');?>
+                <?php echo form_error('password');?>
+                <?php echo form_password('password','','class="form-control"');?>
+            </div>
+            <div class="form-group">
+                <label>
+                    <?php echo form_checkbox('remember','1',FALSE);?> Remember me
+                </label>
+            </div>
+            <?php echo form_submit('submit', 'Log in', 'class="btn btn-primary btn-lg btn-block"');?>
+            <?php echo form_close();?>
+        </div>
+    </div>
 </div>
